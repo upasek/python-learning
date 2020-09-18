6e98d53c8459a4059c2b71eb60acf335875fdb41
kiran santosh upase <kvsupase@gmail.com>
2020-09-18 19:06:19 +0530

    area and volume

---
+++ src/sq_cu.py
@@ -0,0 +1,8 @@
+#Write a Python program to print the square and cube symbol in the area of a rectangle and volume of a cylinder.
+
+area = 1256.66
+volume = 1254.725
+decimals = 2
+print("The area of the rectangle is {:.{}f}cm\u00b2".format(area,decimals))
+decimals = 3
+print("The volume of the cylinder is {:.{}f}cm\u00b3".format(volume,decimals))
