8857c64f05390253e0cefc062f3d94218ca10bfd
kiran santosh upase <kvsupase@gmail.com>
2020-12-28 08:43:06 +0530

    iterate over elements repeating each as many times as its count.

---
+++ collections/collections_iterate.py
@@ -0,0 +1,7 @@
+#Write a Python program that iterate over elements repeating each as many times as its count.
+
+from collections import Counter
+
+c = Counter(p = 4, q = 2, r = 0, s = -2)
+
+print("New list :",list(c.elements()))
