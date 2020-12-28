d87c804e10e91948f9ea0d34c410d6aa6f959b14
kiran santosh upase <kvsupase@gmail.com>
2020-12-28 08:41:19 +0530

    create a new deque

---
+++ collections/collections_deque.py
@@ -0,0 +1,10 @@
+#Write a Python program to create a new deque with three items and iterate over the deque's elements.
+
+from collections import deque
+
+dq = deque('kiran')
+
+print(dq)
+
+for m in dq:
+    print(m)
