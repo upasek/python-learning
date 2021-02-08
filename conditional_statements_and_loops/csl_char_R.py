09957f9c64f6d3ba5d6bb0e1cccfc4752d87e167
kiran santosh upase <kvsupase@gmail.com>
2021-02-08 23:50:51 +0530

    print alphabet pattern 'R'

---
+++ conditional_statements_and_loops/csl_char_R.py
@@ -0,0 +1,21 @@
+#Write a Python program to print alphabet pattern 'R'.
+
+string = ''
+print("Expected output : ")
+
+for r in range(7):
+    for c in range(7):
+        if (r == 0 or r == 3) and ( c > 0 and c < 5):
+            string = string + '*'
+
+        elif (r == 1 or r == 2) and ( c == 1 or c == 5):
+            string = string + '*'
+
+        elif (c == 1 or c == r-1 ) and ( r >= 4):
+            string = string + '*'
+        else:
+            string = string + ' '
+
+    string = string + '\n'
+
+print(string)
