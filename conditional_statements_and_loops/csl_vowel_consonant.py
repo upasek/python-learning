026c73fe8369254bffb3f78cfd80fb152648cffa
kiran santosh upase <kvsupase@gmail.com>
2021-03-12 23:21:54 +0530

    program to check whether an alphabet is a vowel or consonant

---
+++ conditional_statements_and_loops/csl_vowel_consonant.py
@@ -0,0 +1,10 @@
+# Write a Python program to check whether an alphabet is a vowel or consonant.
+
+let = input("Input a letter of the alphabet : ")
+
+cap = 'AEIOU'
+sam = 'aeiou'
+if let in cap or let  in sam:
+    print("%s is a vowel."%let)
+else:
+    print("%s is a consonant."%let)
